5049d7790ecf106da168446ee03d0d872f6865b8
danielmorelo <jmorelo@lasillavacia.com>
2017-05-25 13:03:24 -0500

    Eventos in process

--- src/js/script.js
+++ src/js/script.js
@@ -1,103 +1,117 @@
 // When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
-var map;
-function init() {
-  // Basic options for a simple Google Map
-  // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-  var mapOptions = {
-    // How zoomed in you want the map to start at (always required)
-    zoom: 11,
+if (typeof google) {
+  google.maps.event.addDomListener(window, 'load', init);
+  var map;
 
-    // The latitude and longitude to center the map (always required)
-    center: new google.maps.LatLng(4.710989, -74.072092), // New York
+  function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    var mapOptions = {
+      // How zoomed in you want the map to start at (always required)
+      zoom: 11,
 
-    // How you would like to style the map.
-    // This is where you would paste any style found on Snazzy Maps.
-    styles: [
-      {
-        "featureType": "administrative",
-        "elementType": "labels.text.fill",
-        "stylers": [
-          {
-            "color": "#444444"
-          }
-        ]
-      },
-      {
-        "featureType": "landscape",
-        "elementType": "all",
-        "stylers": [
-          {
-            "color": "#f2f2f2"
-          }
-        ]
-      },
-      {
-        "featureType": "poi",
-        "elementType": "all",
-        "stylers": [
-          {
-            "visibility": "off"
-          }
-        ]
-      },
-      {
-        "featureType": "road",
-        "elementType": "all",
-        "stylers": [
-          {
-            "saturation": -100
-          },
-          {
-            "lightness": 45
-          }
-        ]
-      },
-      {
-        "featureType": "road.highway",
-        "elementType": "all",
-        "stylers": [
-          {
-            "visibility": "simplified"
-          }
-        ]
-      },
-      {
-        "featureType": "road.arterial",
-        "elementType": "labels.icon",
-        "stylers": [
-          {
-            "visibility": "off"
-          }
-        ]
-      },
-      {
-        "featureType": "transit",
-        "elementType": "all",
-        "stylers": [
-          {
-            "visibility": "off"
-          }
-        ]
-      },
-      {
-        "featureType": "water",
-        "elementType": "all",
-        "stylers": [
-          {
-            "color": "#46bcec"
-          },
-          {
-            "visibility": "on"
-          }
-        ]
-      }
-    ]
-  };
+      // The latitude and longitude to center the map (always required)
+      center: new google.maps.LatLng(4.710989, -74.072092), // New York
 
-  // Get the HTML DOM element that will contain your map
-  // We are using a div with id="map" seen below in the <body>
-  var mapElement = document.getElementById('map');
-  // Create the Google Map using our element and options defined above
-  map = new google.maps.Map(mapElement, mapOptions);
+      // How you would like to style the map.
+      // This is where you would paste any style found on Snazzy Maps.
+      styles: [
+        {
+          "featureType": "administrative",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#444444"
+            }
+          ]
+        },
+        {
+          "featureType": "landscape",
+          "elementType": "all",
+          "stylers": [
+            {
+              "color": "#f2f2f2"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "all",
+          "stylers": [
+            {
+              "saturation": -100
+            },
+            {
+              "lightness": 45
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "all",
+          "stylers": [
+            {
+              "visibility": "simplified"
+            }
+          ]
+        },
+        {
+          "featureType": "road.arterial",
+          "elementType": "labels.icon",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "transit",
+          "elementType": "all",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "all",
+          "stylers": [
+            {
+              "color": "#46bcec"
+            },
+            {
+              "visibility": "on"
+            }
+          ]
+        }
+      ]
+    };
+
+    // Get the HTML DOM element that will contain your map
+    // We are using a div with id="map" seen below in the <body>
+    var mapElement = document.getElementById('map');
+    if (!mapElement) return;
+    // Create the Google Map using our element and options defined above
+    map = new google.maps.Map(mapElement, mapOptions);
+  }
 }
+
+// The publicaciones toggles
+$(document).ready(function () {
+  $('.publicacion').each(function () {
+    var $this = $(this);
+    $this.find('.publicacion-toggle').click(function () {
+      $this.find('.publicaciones__content').slideToggle("slow");
+    });
+  });
+});
